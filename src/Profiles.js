ae94f43ed4aa6b43097f3bab064cfea068d30eba
Tapuwan <68889258+JeongTaehwan@users.noreply.github.com>
2021-06-09 16:35:22 +0900

    [ADD] NavLink 추가
    [ADD] NavLink 추가

--- src/Profiles.js
+++ src/Profiles.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Profile from './Profile';
-import { Link, Route } from 'react-router-dom';
+import { NavLink, Route } from 'react-router-dom';
 
 
 function Profiles() {
@@ -8,8 +8,13 @@ function Profiles() {
         <div>
             <h3>사용자 목록</h3>
             <ul>
-                <li><Link to="/profiles/taehwan">taehwan</Link></li>
-                <li><Link to="/profiles/homer">homer</Link></li>
+                <li><NavLink
+                    to="/profiles/taehwan"
+                    activeStyle={{ background: 'black', color: 'white' }}>
+                    taehwan
+                        </NavLink></li>
+                <li><NavLink to="/profiles/homer" activeStyle={{ background: 'black', color: 'white' }}>
+                    homer</NavLink></li>
             </ul>
             <Route path="/profiles" exact render={() => <div>사용자를 선택해주세요</div>} />
             <Route path="/profiles/:username" component={Profile} />
